c4716d481797eaa9d2f3f76e8459709429e3c683
= <=>
2022-02-20 11:44:06 +0530

    mobile font size

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,18 @@
 import Preview from "./componets/preview";
 import Mainsection from "./componets/mainsection";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="left">
-        <Preview />
+    <>
+      <div className="container">
+        <div className="left">
+          <Preview />
+        </div>
+        <div className="right">
+          <Mainsection />
+        </div>
       </div>
-      <div className="right">
-        <Mainsection />
-      </div>
-    </div>
+    </>
   );
 }
