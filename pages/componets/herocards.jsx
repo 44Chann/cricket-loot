5f87a53e81572b90d8a4b18bfed103d9a501af39
= <=>
2022-02-21 20:24:26 +0530

    minner changes done

--- pages/componets/herocards.jsx
+++ pages/componets/herocards.jsx
@@ -3,7 +3,7 @@ import HeroCard from "./Herocard";
 const HeroCards = () => {
   return (
     <div className="hero-cards">
-      <HeroCard img="/images/supply.svg" name="SUPPLY" value="10,000 ITEMS" />
+      <HeroCard img="/images/supply.svg" name="SUPPLY" value="2022 ITEMS" />
       <HeroCard img="/images/cost.svg" name="COST" value="20 MATIC" />
       <HeroCard img="/images/wen.svg" name="WHEN" value="28TH FEB" />
     </div>
