b56141b633cc212d55333f548f17d19e41fdf3b6
= <=>
2022-02-21 18:25:33 +0530

    input box added

--- pages/componets/mintbtn.jsx
+++ pages/componets/mintbtn.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
 const Mintbtn = () => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    return setValue(e.target.value);
+  };
+
   return (
-    <button className="container-btn">
-      <img src="/images/mint.svg" alt="" />
-    </button>
+    <>
+      <div className="mint">
+        <input
+          className="mintinput"
+          type="text"
+          placeholder="Token"
+          value={value}
+          onChange={handleChange}
+        />
+
+        <button className="container-btn">
+          <img src="/images/mint.svg" alt="" />
+        </button>
+      </div>
+    </>
   );
 };
 
