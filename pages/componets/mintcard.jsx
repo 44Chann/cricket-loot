06952b9abc7e4fb6561f7ef98c924d4d124a06a1
= <=>
2022-02-21 20:11:34 +0530

    mint card removed

--- pages/componets/mintcard.jsx
+++ pages/componets/mintcard.jsx
@@ -3,7 +3,7 @@ import Mintbtn from "./mintbtn";
 const MintSection = () => {
   return (
     <div className="mint-section">
-      <div className="mintcard"></div>
+      {/* <div className="mintcard"></div> */}
       <div className="mintbtn">
         <Mintbtn />
       </div>
